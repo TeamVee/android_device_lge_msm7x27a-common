e476a50bb470c6bc5dcd4c8336fe2e9278238b23
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-05-10 20:46:45 -0300

    msm7x27a: art: We need to partially pre optimize
    "If a device has both limited space in the system and data partitions, compiler filter flags can be used to disable compilation of certain apps. This will save space in both system and data, as there won’t be any compiled code, but these apps will have to be interpreted. This example configuration would pre-optimize the boot classpath but prevent compilation of other apps that are not prebuilts. However, to prevent noticeable performance degradation of system_server, the services.jar is still compiled but optimized for space. Note that user-installed applications will still use the default compiler filter of speed."
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -185,6 +185,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags="--no-watch-dog" \
     dalvik.vm.image-dex2oat-filter="speed"
 
+PRODUCT_DEX_PREOPT_DEFAULT_FLAGS := --compiler-filter=interpret-only
+$(call add-product-dex-preopt-module-config,services,--compiler-filter=space)
+
 # Force Camera Portability API, since newer camera API isn't supported.
 PRODUCT_PROPERTY_OVERRIDES += camera2.portability.force_api="1"
 
