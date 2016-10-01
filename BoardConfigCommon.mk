1a5dd08a771e13692c199d67af5a0713cdcad2b3
Aidas Lukošius <aidaslukosius75@gmail.com>
2016-10-01 16:52:36 +0300

    Optimize HWUI for performance

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,6 +58,7 @@ COMMON_GLOBAL_CFLAGS += -DLPA_DEFAULT_BUFFER_SIZE=480
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+HWUI_COMPILE_FOR_PERF := true
 
 # Add h/w acceleration in browser
 WITH_JIT := true
